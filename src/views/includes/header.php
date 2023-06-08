0c1a789d09c4d98b6e95abc2dcdc6d74f89915a4
chloebourgois <chloebourgois@gmail.com>
2023-06-08 23:27:35 +0200

    body tag opened sooner in header.php

--- src/views/includes/header.php
+++ src/views/includes/header.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="../../../public/assets/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
+<body>
 <header class="fixed-top">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand logo" href="/"><img src="../../public/assets/img/logo.png" alt="logo"></a>
@@ -38,5 +39,3 @@
         </div>
     </nav>
 </header>
-<body>
-
